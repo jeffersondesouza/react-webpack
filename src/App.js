847af566c4880b507050b0f7895034f5a173a14f
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-01-11 15:55:14 -0300

    header ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { Component, Fragment } from 'react';
-import axios from 'axios';
-
 import './style/sass/main.scss';
-import Form from './components/forms/FormWrapper/Form';
-import Reservation from './components/Reservation';
-
 
 
 import {
+  Form,
   ErrorMessage,
   DebugForm,
   InputField,
@@ -15,7 +11,7 @@ import {
   FormGroup
 } from './components/forms/FormWrapper';
 
-
+import Header from './components/layout/Header';
 
 
 export default class App extends Component {
@@ -31,68 +27,59 @@ export default class App extends Component {
 
   }
 
-  handleSubmit = (v)=>{
+  handleSubmit = (v) => {
     console.log('v', v);
   }
-  
+
   render() {
     const { values } = this.state;
 
-
-
     return (
       <div>
-        <h1>Hello world</h1>
-        <i className="icon-rebel"></i>
+        <Header />
         <FormGroup initialValues={values} validate={this.validate} onSubmit={this.handleSubmit}>
-          {(formProps) => {
+          {(formProps) =>
 
-            return (
-              <Form {...formProps}>
+            <Form {...formProps}>
+              <div>
+                <label>Email</label>
+                <InputField name="name" {...formProps} />
+                <ErrorMessage  {...formProps} name="email" />
+              </div>
 
-                <div>
-                  <label>Email</label>
-                  <InputField name="name" {...formProps} />
-                  <ErrorMessage  {...formProps} name="email" />
-                </div>
+              <div className="form-group">
+                <label>Fones</label>
+                <InputFieldArray name="phones" {...formProps} >
+                  {(inputArrayProps) => {
+                    const { arrayHelpers, handleAddMore } = inputArrayProps;
 
-
-                <div className="form-group">
-                  <label>Fones</label>
-                  <InputFieldArray name="phones" {...formProps} >
-                    {(inputArrayProps) => {
-                      const { arrayHelpers, handleAddMore } = inputArrayProps;
-
-                      return (
-                        <React.Fragment>
-                          {
-                            arrayHelpers.map((item, i) => (
-                              !item[`destroy.${i}`] && <div key={i} >
-                                <InputField {...inputArrayProps} type="number" name={`code.${i}`} values={item} />
-                                <InputField {...inputArrayProps} type="number" name={`number.${i}`} values={item} />
-                                <InputField {...inputArrayProps} type="checkbox" name={`destroy.${i}`} values={item} />
-                              </div>
-                            ))
-                          }
-                          <button
-                            type="button"
-                            className="btn btn--block"
-                            onClick={() => handleAddMore('code', 'number', 'destroy')}>
-                            + Add more
+                    return (
+                      <Fragment>
+                        {
+                          arrayHelpers.map((item, i) => (
+                            !item[`destroy.${i}`] && <div key={i} >
+                              <InputField {...inputArrayProps} type="number" name={`code.${i}`} values={item} />
+                              <InputField {...inputArrayProps} type="number" name={`number.${i}`} values={item} />
+                              <InputField {...inputArrayProps} type="checkbox" name={`destroy.${i}`} values={item} />
+                            </div>
+                          ))
+                        }
+                        <button
+                          type="button"
+                          className="btn btn--block"
+                          onClick={() => handleAddMore('code', 'number', 'destroy')}>
+                          + Add more
                           </button>
-                        </React.Fragment>
-                      );
-                    }
-                    }
-                  </InputFieldArray>
-                  <button>submit</button>
-                </div>
-
-
-                <DebugForm {...formProps} />
-              </Form>
-            )
-          }}
+                      </Fragment>
+                    );
+                  }
+                  }
+                </InputFieldArray>
+                <button>submit</button>
+              </div>
+              <DebugForm {...formProps} />
+            </Form>
+          }
         </FormGroup>
 
       </div>
---
+++ src/components/layout/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.scss';
+const Header = () => (
+  <header >
+    <div className="header__title">
+      <i className="icon-rebel"></i>
+      <h1 className="header__primary-heading">React BoilerForce</h1>
+      <i className="icon-empire"></i>
+    </div>
+  </header>
+);
+
+export default Header;
